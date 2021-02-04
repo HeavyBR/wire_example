79e36f255f6242c46d25d42a8c28618a1db5e701
Matheus Cumpian <matheus.cumpian@sensedia.com>
2021-02-04 18:06:11 -0300

    First commit

---
+++ wire.go
@@ -0,0 +1,14 @@
+//+build wireinject
+
+package main
+
+import (
+	"github.com/google/wire"
+)
+
+
+func initializeEvent(msg string) (event, error) {
+	wire.Build(newMessage, newGreeter, newEvent)
+
+	return event{}, nil
+}
